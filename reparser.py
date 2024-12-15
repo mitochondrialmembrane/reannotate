93f50358d9d06db402f6c71b307724c1d58059d0
Alex Jiang <alexander_jiang@brown.edu>
2024-12-14 22:15:02 -0500

    fixed mp4 stuff

--- reparser.py
+++ reparser.py
@@ -7,16 +7,21 @@ root_folder = "reannotate/reannotation_tmp"
 for dirpath, dirnames, filenames in os.walk(root_folder):
     for file in filenames:
         if file.endswith(".json"):  # Check if the file is a JSON file
-            file_path = os.path.join(dirpath, file)  # Get the full path to the file
-            
-            with open(file_path, "r") as infile:
-                d = json.load(infile)
-            for a in d:
-                x = a["annotation"].split("\t")
-                if len(x) > 1:
-                    a["annotation"] = x[1]
+            try:
+                file_path = os.path.join(dirpath, file)  # Get the full path to the file
+                
+                with open(file_path, "r") as infile:
+                    d = json.load(infile)
 
-            json_object = json.dumps(d, indent=4)
+                for a in d:
+                    print(a)
+                    x = a["annotation"].split("\t")
+                    if len(x) > 1:
+                        a["annotation"] = x[1]
 
-            with open(file_path, "w") as outfile:
-                outfile.write(json_object)
+                json_object = json.dumps(d, indent=4)
+
+                with open(file_path, "w") as outfile:
+                    outfile.write(json_object)
+            except:
+                continue
